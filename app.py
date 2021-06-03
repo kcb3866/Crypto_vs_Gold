13030702b1f397725619279cf5a9908a1a2471e4
vaughnsmith99 <vaughnsmith99@gmail.com>
2021-06-03 14:40:50 -0400

    Added Plotly functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect
 import requests
 import json
+import pandas as pd
+import plotly
+import plotly.express as px
 from config import API_KEY, CRYPTO_ROUTE, GOLD_ROUTE
 from tensorflow.keras.models import load_model
 
@@ -18,9 +21,18 @@ def index():
     crypto_list = json.loads(crypto_request)
     gold_list = json.loads(gold_request)
     crypto,gold = ML_Forecast(crypto_list), ML_Forecast(gold_list)
+    df = pd.DataFrame({
+        "Crypto": crypto,
+        "Gold": gold})
+    fig = px.line(df, x=df["Datetime"], y=df["Crypto Price", "Gold Price"])
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template("index.html", crypto=crypto, gold=gold)
+    return render_template('templates/index.html', graphJSON=graphJSON)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+def notdash():
+
