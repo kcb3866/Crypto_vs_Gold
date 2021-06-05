2efc805cda3c8644c9f83b01de448bcf11d8b50e
Davis Knowles <dknowles52@gmail.com>
2021-06-05 09:31:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,41 +68,11 @@ print(f'The R2 Score is {r2_score}')
 #################################################
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
-
-# from flask_sqlalchemy import SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
-# # Remove tracking modifications
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
-# Pet = create_classes(db)
-
 # create route that renders index.html template
 @app.route("/")
 def home():
 
     return render_template("index.html")
-
-
-# # Query the database and send the jsonified results
-# @app.route("/send", methods=["GET", "POST"])
-# def send():
-#     if request.method == "POST":
-#         name = request.form["petName"]
-#         lat = request.form["petLat"]
-#         lon = request.form["petLon"]
-
-#         pet = Pet(name=name, lat=lat, lon=lon)
-#         db.session.add(pet)
-#         db.session.commit()
-#         return redirect("/", code=302)
-
-#     return render_template("form.html")
 
 
 @app.route("/api/btc")
@@ -111,15 +81,6 @@ def btc_pred():
     result = [{
         "y_pred": y_pred.squeeze().values.tolist(),
         "y_test": y_test.squeeze().values.tolist(),
-        # "text": hover_text,
-        # "hoverinfo": "text",
-        # "marker": {
-        #     "size": 50,
-        #     "line": {
-        #         "color": "rgb(8,8,8)",
-        #         "width": 1
-            # },
-        # }
     }]
 
     return jsonify(result)
