ae3749114735a900129fbb3e5da80e0a74ec51de
Davis Knowles <dknowles52@gmail.com>
2021-06-05 09:17:28 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,83 +1,125 @@
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 500;
+// Uncomment below
+function buildPlot() {
+  /* data route */
+  const url = "/api/btc";
+  d3.json(url).then(function(response) {
 
-// Define the chart's margins as an object
-var margin = {
-  top: 60,
-  right: 60,
-  bottom: 60,
-  left: 60
-};
+     console.log(response[0]);
 
-// Define dimensions of the chart area
-var chartWidth = svgWidth - margin.left - margin.right;
-var chartHeight = svgHeight - margin.top - margin.bottom;
+        var y_pred = response[0].y_pred;
+        var y_test = response[0].y_test;
+        var dates = y_pred.Date;
+        console.log(dates);
 
-// Select body, append SVG area to it, and set its dimensions
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+        var trace1 = {
+            type: "line",
+            x: dates,
+            y: y_test
+        };
 
-// Append a group area, then set its margins
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        var trace2 = {
+            type: "line",
+            x: dates,
+            y: y_pred
+        };
 
-// Configure a parseTime function which will return a new Date object from a string
-var parseTime = d3.timeParse("%Y");
 
-// Load data from URL
-d3.json('https://dknowles52-app1.herokuapp.com/api/btc').then(function(forceData) {
+        var data = [trace1, trace2];
+    
+        var layout = {
+          title: "BTC Chart",
+        };
 
-  // Print the forceData
-  console.log(forceData);
+        Plotly.newPlot('plot', data, layout);
+    });
+}
 
-  // Format the date and cast the force value to a number
-  forceData.forEach(function(data) {
-    data.date = parseTime(data.date);
-    data.force = +data.force;
-  });
+buildPlot();
 
-  // Configure a time scale
-  // d3.extent returns the an array containing the min and max values for the property specified
-  var xTimeScale = d3.scaleTime()
-    .domain(d3.extent(forceData, data => data.date))
-    .range([0, chartWidth]);
 
-  // Configure a linear scale with a range between the chartHeight and 0
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(forceData, data => data.force)])
-    .range([chartHeight, 0]);
 
-  // Create two new functions passing the scales in as arguments
-  // These will be used to create the chart's axes
-  var bottomAxis = d3.axisBottom(xTimeScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
 
-  // Configure a line function which will plot the x and y coordinates using our scales
-  var drawLine = d3.line()
-    .x(data => xTimeScale(data.date))
-    .y(data => yLinearScale(data.force));
 
-  // Append an SVG path and plot its points using the line function
-  chartGroup.append("path")
-    // The drawLine function returns the instructions for creating the line for forceData
-    .attr("d", drawLine(forceData))
-    .classed("line", true);
+// // Define SVG area dimensions
+// var svgWidth = 960;
+// var svgHeight = 500;
 
-  // Append an SVG group element to the chartGroup, create the left axis inside of it
-  chartGroup.append("g")
-    .classed("axis", true)
-    .call(leftAxis);
+// // Define the chart's margins as an object
+// var margin = {
+//   top: 60,
+//   right: 60,
+//   bottom: 60,
+//   left: 60
+// };
 
-  // Append an SVG group element to the chartGroup, create the bottom axis inside of it
-  // Translate the bottom axis to the bottom of the page
-  chartGroup.append("g")
-    .classed("axis", true)
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-}).catch(function(error) {
-  console.log('Error Found: ');
-  console.log(error);
-});
+// // Define dimensions of the chart area
+// var chartWidth = svgWidth - margin.left - margin.right;
+// var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// // Select body, append SVG area to it, and set its dimensions
+// var svg = d3.select("body")
+//   .append("svg")
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight);
+
+// // Append a group area, then set its margins
+// var chartGroup = svg.append("g")
+//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// // Configure a parseTime function which will return a new Date object from a string
+// var parseTime = d3.timeParse("%Y");
+
+// // Load data from URL
+// d3.json('https://dknowles52-app1.herokuapp.com/api/btc').then(function(forceData) {
+
+//   // Print the forceData
+//   console.log(forceData);
+
+//   // Format the date and cast the force value to a number
+//   forceData.forEach(function(data) {
+//     data.date = parseTime(data.date);
+//     data.force = +data.force;
+//   });
+
+//   // Configure a time scale
+//   // d3.extent returns the an array containing the min and max values for the property specified
+//   var xTimeScale = d3.scaleTime()
+//     .domain(d3.extent(forceData, data => data.date))
+//     .range([0, chartWidth]);
+
+//   // Configure a linear scale with a range between the chartHeight and 0
+//   var yLinearScale = d3.scaleLinear()
+//     .domain([0, d3.max(forceData, data => data.force)])
+//     .range([chartHeight, 0]);
+
+//   // Create two new functions passing the scales in as arguments
+//   // These will be used to create the chart's axes
+//   var bottomAxis = d3.axisBottom(xTimeScale);
+//   var leftAxis = d3.axisLeft(yLinearScale);
+
+//   // Configure a line function which will plot the x and y coordinates using our scales
+//   var drawLine = d3.line()
+//     .x(data => xTimeScale(data.date))
+//     .y(data => yLinearScale(data.force));
+
+//   // Append an SVG path and plot its points using the line function
+//   chartGroup.append("path")
+//     // The drawLine function returns the instructions for creating the line for forceData
+//     .attr("d", drawLine(forceData))
+//     .classed("line", true);
+
+//   // Append an SVG group element to the chartGroup, create the left axis inside of it
+//   chartGroup.append("g")
+//     .classed("axis", true)
+//     .call(leftAxis);
+
+//   // Append an SVG group element to the chartGroup, create the bottom axis inside of it
+//   // Translate the bottom axis to the bottom of the page
+//   chartGroup.append("g")
+//     .classed("axis", true)
+//     .attr("transform", `translate(0, ${chartHeight})`)
+//     .call(bottomAxis);
+// }).catch(function(error) {
+//   console.log('Error Found: ');
+//   console.log(error);
+// });
